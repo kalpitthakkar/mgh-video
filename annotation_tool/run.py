dc2e51a2e2b52750bf5104366a8848d25b253a51
kalpitthakkar <kthakkar1605@gmail.com>
2019-05-31 16:08:21 -0400

    Add default args as well

--- annotation_tool/run.py
+++ annotation_tool/run.py
@@ -5,7 +5,9 @@ from annotate_gui import AnnotationGUI
 if __name__ == '__main__':
     data_dir = '/home/kalpitthakkar/g17_mount/Kalpit/bootstrap/4'
     output_dir = '/home/kalpitthakkar/g17_mount/Kalpit/bootstrap/4'
-    gui = AnnotationGUI(data_dir=data_dir, output_dir=output_dir)
+    gui = AnnotationGUI(data_dir=data_dir, output_dir=output_dir,
+            data_ext='avi', with_annots=True,
+            annots_file_ext='csv', yaml_config='mgh.yaml')
 
     root = gui.build_gui()
     root.mainloop()
